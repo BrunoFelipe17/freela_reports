20cb92b48575dfc0185a0c3c0d464da04ec4a134
BrunoFelipe17 <bruno.felipe7170@gmail.com>
2021-07-11 18:39:40 -0300

    Using Stream.map on the file stream

--- lib/parser.ex
+++ lib/parser.ex
@@ -2,12 +2,13 @@ defmodule GenReport.Parser do
   def parse_csv(file_name) do
     "report/#{file_name}"
     |> File.stream!()
-    |> Enum.map(&parse_line(&1))
+    |> Stream.map(&parse_line(&1))
   end
 
   defp parse_line(line) do
     line
     |> String.trim()
     |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
   end
 end
