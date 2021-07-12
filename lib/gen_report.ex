ec55c6718a68717f4ea32a7c0eb1f40d3827fd44
BrunoFelipe17 <bruno.felipe7170@gmail.com>
2021-07-12 08:40:41 -0300

    Removing the map creator, instead we merge maps in the sum_values function

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -3,6 +3,40 @@ defmodule GenReport do
 
   def build(file_name) do
     file_name
-    |> Parser.parse_csv()
+    |> Parser.parse_file()
+    |> Enum.reduce(create_map(%{}, %{}, %{}), fn line, report -> sum_values(line, report) end)
+  end
+
+  defp sum_values(
+         [name, hours, _day, month, year],
+         %{
+           "all_hours" => all_hours,
+           "hours_per_month" => hours_per_month,
+           "hours_per_year" => hours_per_year
+         }
+       ) do
+    all_hours = merge_maps(all_hours, %{name => hours})
+    hours_per_month = merge_multi_maps(hours_per_month, %{name => %{month => hours}})
+    hours_per_year = merge_multi_maps(hours_per_year, %{name => %{year => hours}})
+
+    create_map(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, v1, v2 -> v1 + v2 end)
+  end
+
+  defp merge_multi_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, v1, v2 ->
+      merge_maps(v1, v2)
+    end)
+  end
+
+  def create_map(all_hours, hours_per_month, hours_per_year) do
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
