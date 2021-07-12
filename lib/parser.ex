f4f1a64c26554d23e85e989d6522735f70929e84
BrunoFelipe17 <bruno.felipe7170@gmail.com>
2021-07-12 08:15:37 -0300

    Converting month to string

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,8 +1,23 @@
 defmodule GenReport.Parser do
-  def parse_csv(file_name) do
+  @months %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
+
+  def parse_file(file_name) do
     "report/#{file_name}"
     |> File.stream!()
-    |> Stream.map(&parse_line(&1))
+    |> Enum.map(&parse_line(&1))
   end
 
   defp parse_line(line) do
@@ -10,6 +25,8 @@ defmodule GenReport.Parser do
     |> String.trim()
     |> String.split(",")
     |> List.update_at(1, &String.to_integer/1)
-    |> List.update_at(3, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, &Map.get(@months, String.to_integer(&1)))
+    |> List.update_at(4, &String.to_integer/1)
   end
 end
